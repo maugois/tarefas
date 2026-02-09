d20310d7366995ab1519d7c9488c7f4140737f04
maugois <gois.mauricio12@gmail.com>
2026-02-09 15:51:57 -0300

    refactor: Adjusting folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import { Header } from "./components/header";
+import { Header } from "../components/header";
 import "./styles/globals.css";
 
 const roboto = Roboto({
